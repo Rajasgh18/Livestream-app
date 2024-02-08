dc21b64f56d13749eda27adde2e102741a4b1b52
Raja Singh <rajasgh18@gmail.com>
2024-02-08 21:03:23 +0530

    all navbar elements have been added with responsiveness

--- app/(browse)/_components/navbar/Logo.tsx
+++ app/(browse)/_components/navbar/Logo.tsx
@@ -11,11 +11,14 @@ const font = Poppins({
 export const Logo = () => {
     return (
         <Link href="/">
-            <div className="hidden lg:flex items-center hover:opacity-75 gap-x-4 transition">
-                <div className="bg-white rounded-full p-1">
+            <div className="flex items-center hover:opacity-75 gap-x-4 transition">
+                <div className="bg-white rounded-full p-1 mr-8 shrink-0 lg:mr-0 lg:shrink">
                     <Image src="/spooky.svg" alt="Gamehub" height={32} width={32} />
                 </div>
-                <div className={cn(font.className)}>
+                <div className={cn(
+                    "hidden lg:block",
+                    font.className
+                    )}>
                     <p className="text-lg font-semibold">Gamehub</p>
                     <p className="text-xs text-muted-foreground">Let&apos;s play</p>
                 </div>
--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -1,3 +1,4 @@
+import { Actions } from "./Actions";
 import { Logo } from "./Logo";
 import { Search } from "./Search";
 
@@ -6,6 +7,7 @@ export const Navbar = () => {
         <nav className="h-20 w-full top-0 fixed z-[49] bg-[#252731] px-2 lg:px-4 flex justify-between items-center shadow-sm">
             <Logo />
             <Search />
+            <Actions/>
         </nav>
     )
 }
