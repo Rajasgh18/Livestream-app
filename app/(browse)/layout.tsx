05e4f7f3771de2cef3706b87d92bf8d55759e365
Raja Singh <rajasgh18@gmail.com>
2024-02-09 20:57:50 +0530

    moved layout to (browse) directory

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { Navbar } from '../_components/navbar'
-import { Sidebar } from '../_components/sidebar'
-import { Container } from '../_components/container'
+import React, { Suspense } from 'react'
+import { Navbar } from './_components/navbar'
+import { Sidebar, SidebarSkeleton } from './_components/sidebar'
+import { Container } from './_components/container'
 
 const BrowserLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
             <Navbar />
             <div className='pt-16 flex h-full'>
-                <Sidebar />
+                <Suspense fallback={<SidebarSkeleton />}>
+                    <Sidebar />
+                </Suspense>
                 <Container>
                     {children}
                 </Container>
