c70009a54a2a2786e1ad5cd828965e0770e1f834
Raja Singh <rajasgh18@gmail.com>
2024-02-10 22:05:00 +0530

    block server action

---
+++ actions/block.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { blockUser, unblockUser } from "@/lib/blocking-service";
+import { revalidatePath } from "next/cache";
+
+export const onBlock = async (id: string) => {
+    const blockedUser = await blockUser(id);
+    revalidatePath('/');
+
+    if (blockedUser) revalidatePath(`/${blockedUser.blocked.username}`)
+
+    return blockedUser;
+};
+
+export const onUnBlock = async (id: string) => {
+    const unBlockedUser = await unblockUser(id);
+
+    revalidatePath('/');
+
+    if (unBlockedUser) revalidatePath(`/${unBlockedUser.blocked.username}`)
+
+    return unBlockedUser;
+};
