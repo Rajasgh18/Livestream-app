6c7ba273b7eee4d53c02926c1ccd64d584febddd
Raja Singh <rajasgh18@gmail.com>
2024-02-10 22:06:43 +0530

    returning 404 if blocked

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -2,6 +2,7 @@ import { isFollowingUser } from "@/lib/follow-service";
 import { getUserByUsername } from "@/lib/user-services";
 import { notFound } from "next/navigation";
 import { Actions } from "./_component/actions";
+import { isBlocked, isBlockedByUser } from "@/lib/blocking-service";
 
 interface UserPageProps {
     params: {
@@ -14,13 +15,17 @@ const UserPage = async ({ params }: UserPageProps) => {
     if (!user) return notFound();
 
     const isFollowing = await isFollowingUser(user.id);
+    const blocked = await isBlocked(user.id);
+    const blockedByUser = await isBlockedByUser(user.id);
+
+    if (blockedByUser) return notFound();
 
     return (
         <div className="h-full space-y-4 flex flex-col items-center justify-center">
             <p>Username: {user.username}</p>
             <p>UserId: {user.id}</p>
             <p>Is Following: {`${isFollowing}`}</p>
-            <Actions isFollowing={isFollowing} userId={user.id} />
+            <Actions isFollowing={isFollowing} isBlocked={blocked} userId={user.id} />
         </div>
     );
 };
