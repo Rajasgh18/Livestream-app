4700e7addfeaa1d0ae8cc2394a0966fd4a644718
Raja Singh <rajasgh18@gmail.com>
2024-02-10 18:08:25 +0530

    User page with follow and unfollow

---
+++ app/(browse)/[username]/_component/actions.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { onFollow, onUnFollow } from "@/actions/follow";
+import { Button } from "@/components/ui/button";
+import { useTransition } from "react";
+import { toast } from "sonner";
+
+interface ActionsProps {
+    isFollowing: boolean;
+    userId: string;
+}
+
+export const Actions = ({ isFollowing, userId }: ActionsProps) => {
+    const [isPending, startTransition] = useTransition();
+
+    const handleFollow = () => {
+        startTransition(() => {
+            onFollow(userId)
+            .then(data => toast.success(`You are now following ${data.following.username}`))
+            .catch(() => toast.error("Something Went Wrong!"));
+        })
+    };
+    const handleUnFollow = () => {
+        startTransition(() => {
+            onUnFollow(userId)
+                .then(data => toast.success(`You have unfollowed ${data.following.username}`))
+                .catch(() => toast.error("Something Went Wrong!"));
+        })
+    };
+
+    const onClick = () => {
+        if (isFollowing)
+            handleUnFollow();
+        else
+            handleFollow();
+    };
+
+    return (
+        <Button disabled={isPending} onClick={onClick}>
+            {isFollowing ? "Unfollow" : "Follow"}
+        </Button>
+    );
+};
--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,13 +1,26 @@
+import { isFollowingUser } from "@/lib/follow-service";
+import { getUserByUsername } from "@/lib/user-services";
+import { notFound } from "next/navigation";
+import { Actions } from "./_component/actions";
+
 interface UserPageProps {
     params: {
         username: string;
     }
 };
 
-const UserPage = ({ params }: UserPageProps) => {
+const UserPage = async ({ params }: UserPageProps) => {
+    const user = await getUserByUsername(params.username);
+    if (!user) return notFound();
+
+    const isFollowing = await isFollowingUser(user.id);
+
     return (
-        <div>
-            User: {params.username}
+        <div className="h-full space-y-4 flex flex-col items-center justify-center">
+            <p>Username: {user.username}</p>
+            <p>UserId: {user.id}</p>
+            <p>Is Following: {`${isFollowing}`}</p>
+            <Actions isFollowing={isFollowing} userId={user.id} />
         </div>
     );
 };
