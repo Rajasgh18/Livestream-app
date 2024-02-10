6305090c78ccb5cc2e48ab95120d6ff7abf9be66
Raja Singh <rajasgh18@gmail.com>
2024-02-10 18:02:46 +0530

    fetching recommendation and ignoring loggedin user

--- lib/recommended-services.ts
+++ lib/recommended-services.ts
@@ -2,10 +2,21 @@ import { db } from "@/lib/db";
 import { getSelf } from "@/lib/auth-services";
 
 export const getRecommended = async () => {
-    const users = await db.user.findMany({
-        orderBy: {
-            createdAt: "desc"
-        }
-    });
+    const self = await getSelf();
+
+    let users = [];
+    if (self.id) {
+        users = await db.user.findMany({
+            where: {
+                NOT: { id: self.id }
+            }
+        })
+    } else {
+        users = await db.user.findMany({
+            orderBy: {
+                createdAt: "desc"
+            }
+        });
+    }
     return users;
 }
