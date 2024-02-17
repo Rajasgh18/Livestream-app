31ee3dc5bd05f30fd10817e481e22add68c9fd80
Raja Singh <rajasgh18@gmail.com>
2024-02-17 17:21:50 +0530

    searching capability in community

--- components/stream-player/chat-community.tsx
+++ components/stream-player/chat-community.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { useParticipants } from "@livekit/components-react";
-import { useState } from "react";
-import { useDebounceValue } from "usehooks-ts";
+import { useMemo, useState } from "react";
+import { useDebounce } from "usehooks-ts";
 import { Input } from "../ui/input";
 import { ScrollArea } from "../ui/scroll-area";
 import { CommunityItem } from "./community-item";
+import { LocalParticipant, RemoteParticipant } from "livekit-client";
 
 interface ChatCommunityProps {
     hostName: string;
@@ -19,7 +20,7 @@ export const ChatCommunity = ({
     isHidden
 }: ChatCommunityProps) => {
     const [value, setValue] = useState("");
-    const debouncedValue = useDebounceValue(value, 500);
+    const debouncedValue = useDebounce<string>(value, 500);
     const participants = useParticipants();
 
     const onChange = (newValue: string) => {
@@ -33,18 +34,31 @@ export const ChatCommunity = ({
             </div>
         );
 
+    const filteredParticipants = useMemo(() => {
+        const deduped = participants.reduce((acc, participant) => {
+            const hostAsViewer = `host-${participant.identity}`;
+            if (!acc.some((p) => p.identity === hostAsViewer)) {
+                acc.push(participant);
+            }
+            return acc;
+        }, [] as (RemoteParticipant | LocalParticipant)[]);
+        return deduped.filter((participant) => {
+            return participant.name?.toLowerCase().includes(debouncedValue.toLowerCase())
+        });
+    }, [participants, debouncedValue]);
+
     return (
         <div className="p-4">
             <Input onChange={e => onChange(e.target.value)} placeholder="Search Community" className="border-white/10" />
             <ScrollArea className="gap-y-2 mt-4">
                 <p className="text-sm text-center text-muted-foreground hidden last:block p-2">No results</p>
-                {participants.map(participant => (
+                {filteredParticipants.map(participant => (
                     <CommunityItem
-                    key={participant.identity}
-                    hostName={hostName}
-                    viewerName={viewerName}
-                    participantName={participant.name}
-                    participantIdentity={participant.identity}
+                        key={participant.identity}
+                        hostName={hostName}
+                        viewerName={viewerName}
+                        participantName={participant.name}
+                        participantIdentity={participant.identity}
                     />
                 ))}
             </ScrollArea>
