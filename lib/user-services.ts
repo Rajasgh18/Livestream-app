92d706d066de0a15b02fc7721b22deee4ce922f2
Raja Singh <rajasgh18@gmail.com>
2024-02-26 21:32:31 +0530

    updated so that it also fetches followercount

--- lib/user-services.ts
+++ lib/user-services.ts
@@ -4,7 +4,12 @@ export const getUserByUsername = async (username: string) => {
     const user = await db.user.findUnique({
         where: { username },
         include: {
-            stream: true
+            stream: true,
+            _count: {
+                select: {
+                    followedBy: true
+                }
+            }
         }
     });
     return user;
