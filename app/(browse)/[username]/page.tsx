7766c1c7008c88e3a1fb5db05345bf45b89d0de8
Raja Singh <rajasgh18@gmail.com>
2024-02-27 20:51:51 +0530

    added stream to userpage

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -3,6 +3,7 @@ import { getUserByUsername } from "@/lib/user-services";
 import { notFound } from "next/navigation";
 import { Actions } from "./_component/actions";
 import { isBlocked, isBlockedByUser } from "@/lib/blocking-service";
+import { StreamPlayer } from "@/components/stream-player";
 
 interface UserPageProps {
     params: {
@@ -12,21 +13,20 @@ interface UserPageProps {
 
 const UserPage = async ({ params }: UserPageProps) => {
     const user = await getUserByUsername(params.username);
-    if (!user) return notFound();
+    if (!user || !user.stream) notFound();
 
     const isFollowing = await isFollowingUser(user.id);
     const blocked = await isBlocked(user.id);
     const blockedByUser = await isBlockedByUser(user.id);
 
-    if (blockedByUser) return notFound();
+    if (blockedByUser) notFound();
 
     return (
-        <div className="h-full space-y-4 flex flex-col items-center justify-center">
-            <p>Username: {user.username}</p>
-            <p>UserId: {user.id}</p>
-            <p>Is Following: {`${isFollowing}`}</p>
-            <Actions isFollowing={isFollowing} isBlocked={blocked} userId={user.id} />
-        </div>
+        <StreamPlayer
+            user={user}
+            stream={user.stream}
+            isFollowing={isFollowing}
+        />
     );
 };
 
