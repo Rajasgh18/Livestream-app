164789ac8b1d2ffbee981b273b13925d87b74002
Raja Singh <rajasgh18@gmail.com>
2024-02-28 22:06:21 +0530

    blocking of guest users

--- actions/block.ts
+++ actions/block.ts
@@ -1,13 +1,34 @@
 "use server";
 
+import { getSelf } from "@/lib/auth-services";
 import { blockUser, unblockUser } from "@/lib/blocking-service";
+import { RoomServiceClient } from "livekit-server-sdk";
 import { revalidatePath } from "next/cache";
 
-export const onBlock = async (id: string) => {
-    const blockedUser = await blockUser(id);
-    revalidatePath('/');
+const roomService = new RoomServiceClient(
+    process.env.LIVEKIT_API_URL!,
+    process.env.LIVEKIT_API_KEY!,
+    process.env.LIVEKIT_API_SECRET!,
+);
 
-    if (blockedUser) revalidatePath(`/${blockedUser.blocked.username}`)
+export const onBlock = async (id: string) => {
+    const self = await getSelf();
+
+    let blockedUser;
+    try {
+        blockedUser = await blockUser(id);
+
+    } catch (error) {
+        // This means user is a guest
+    }
+
+    try {
+        await roomService.removeParticipant(self.id, id);
+    } catch (error) {
+        // This means user is not in the room
+    }
+
+    revalidatePath(`/u/${self.username}/community`);
 
     return blockedUser;
 };
